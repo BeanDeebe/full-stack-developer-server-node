c87fe18b7ce7825a9e427551672e8bb5a7e765ec
Dean Beebe <beebe.d@northeastern.edu>
2022-04-04 13:45:50 -0400

    A8 4 done

--- full-stack-developer-server-node/server.js
+++ full-stack-developer-server-node/server.js
@@ -3,6 +3,6 @@ import helloController from "./controllers/hello-controller.js";
 import userController from "./controllers/user-controller.js";
 
 const app = express();
-helloController(app);
+app.use(express.json());
 userController(app);
 app.listen(4000);
