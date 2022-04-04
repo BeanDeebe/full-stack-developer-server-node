73c8c9b480b09d5cf168f10c6693f9d64468ff02
Dean Beebe <beebe.d@northeastern.edu>
2022-04-04 14:56:49 -0400

    A8 assignment

--- full-stack-developer-server-node/server.js
+++ full-stack-developer-server-node/server.js
@@ -1,8 +1,10 @@
-import express from 'express'
+import express from 'express';
+import cors from 'cors';
 import usersController from "./controllers/users-controller.js";
 import tuitsController from "./controllers/tuits-controller.js";
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 usersController(app);
 tuitsController(app);
