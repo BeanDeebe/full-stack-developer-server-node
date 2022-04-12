3e2a77ca3c0f4f4d46cabf2c7c74aeac5ebc6d2e
Dean Beebe <beebe.d@northeastern.edu>
2022-04-12 14:53:26 -0400

    A9 assignment

--- full-stack-developer-server-node/server.js
+++ full-stack-developer-server-node/server.js
@@ -2,8 +2,10 @@ import express from 'express';
 import cors from 'cors';
 import usersController from "./controllers/users-controller.js";
 import tuitsController from "./controllers/tuits-controller.js";
+import mongoose from 'mongoose';
 import helloController from "./controllers/hello-controller.js";
 
+mongoose.connect('mongodb://localhost:27017/webdev');
 const app = express();
 app.use(cors());
 app.use(express.json());
